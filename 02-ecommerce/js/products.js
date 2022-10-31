e0ffa825245b0a58ef755d8fc88fce9a99a51961
Nimeavles <prlf505@gmail.com>
2022-10-31 11:46:06 +0100

    add: prices withs bargains

--- 02-ecommerce/js/products.js
+++ 02-ecommerce/js/products.js
@@ -3,65 +3,78 @@ export const products = [
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
   {
     src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
     description: "Women's Classic Tee",
     name: "PintaAPIs Developers",
+    price: 15
   },
 ];
--- 02-ecommerce/js/showProducts.js
+++ 02-ecommerce/js/showProducts.js
@@ -2,22 +2,36 @@ import { products } from "./products";
 
 const $ = element => document.querySelector(element);
 
-function createNodeOnDom(src, description, name) {
+function createNodeOnDom(src, description, name, price) {
     const product = document.createElement('div');
     const image = document.createElement('img');
     const descriptionProduct = document.createElement('div');
     const productParagraph = document.createElement('p');
     const productName = document.createElement('h3');
+    const priceSpan = document.createElement('span');
+    const pricesDiv = document.createElement('div');
+    const priceParagraph = document.createElement('p');
+    const newPrice = document.createElement('p');
 
     product.setAttribute('class', 'product-card');
     descriptionProduct.setAttribute('class', 'description-product');
+    priceParagraph.setAttribute('class', 'old-price');
+    newPrice.setAttribute('class', 'new-price');
 
     image.src = src;
     productParagraph.innerHTML = description;
     productName.innerHTML = name;
+    priceParagraph.innerHTML = `${price}$`;
+    newPrice.innerHTML = `${ price - 3 }$`;
+
+    pricesDiv.appendChild(priceParagraph);
+    pricesDiv.appendChild(newPrice);
+
+    priceSpan.appendChild(pricesDiv);
+    priceSpan.appendChild(productParagraph);
 
     descriptionProduct.appendChild(productName);
-    descriptionProduct.appendChild(productParagraph);
+    descriptionProduct.appendChild(priceSpan);
     
     product.appendChild(image);
     product.appendChild(descriptionProduct);
@@ -25,6 +39,6 @@ function createNodeOnDom(src, description, name) {
     return product
 }
 
-products.map(({ src, description, name }) => {
-    $('.products-grid').appendChild(createNodeOnDom(src, description, name))
+products.map(({ src, description, name, price }) => {
+    $('.products-grid').appendChild(createNodeOnDom(src, description, name, price))
 })
