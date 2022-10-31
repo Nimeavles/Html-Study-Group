d61f3fc4abda88c9bfc2a77ccbc3d1eb93bc7399
Nimeavles <prlf505@gmail.com>
2022-10-31 10:33:57 +0100

    upload files for the start of the class

--- 02-ecommerce/js/products.js
+++ 02-ecommerce/js/products.js
@@ -1,50 +1,67 @@
 export const products = [
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    },
-    {
-        src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
-        description: "Women's Classic Tee"
-    }
-]
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+  {
+    src: "https://vangogh.teespring.com/v3/image/wYtlEYUh0mBVL4uM4R2Ot3k91cM/800/800.jpg",
+    description: "Women's Classic Tee",
+    name: "PintaAPIs Developers",
+  },
+];
--- 02-ecommerce/js/showProducts.js
+++ 02-ecommerce/js/showProducts.js
@@ -2,22 +2,29 @@ import { products } from "./products";
 
 const $ = element => document.querySelector(element);
 
-function createNodeOnDom(src, description) {
+function createNodeOnDom(src, description, name) {
     const product = document.createElement('div');
     const image = document.createElement('img');
-    const descriptionProduct = document.createElement('p');
+    const descriptionProduct = document.createElement('div');
+    const productParagraph = document.createElement('p');
+    const productName = document.createElement('h3');
 
     product.setAttribute('class', 'product-card');
+    descriptionProduct.setAttribute('class', 'description-product');
 
     image.src = src;
-    descriptionProduct.innerHTML = description;
+    productParagraph.innerHTML = description;
+    productName.innerHTML = name;
 
+    descriptionProduct.appendChild(productName);
+    descriptionProduct.appendChild(productParagraph);
+    
     product.appendChild(image);
     product.appendChild(descriptionProduct);
 
     return product
 }
 
-products.map( ({ src, description }) => {
-    $('.products-grid').appendChild(createNodeOnDom(src, description))
+products.map(({ src, description, name }) => {
+    $('.products-grid').appendChild(createNodeOnDom(src, description, name))
 })
